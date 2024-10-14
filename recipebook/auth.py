32faa87bbb2e8e9933abd561ba17d267237b58fd
magoninho <magoninhogamer@gmail.com>
2024-10-14 18:47:29 -0300

    fixed import

--- recipebook/auth.py
+++ recipebook/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_required, logout_user, current_user, login_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from .models import User
+from . import db
 
 bp = Blueprint('auth', __name__)    
 
